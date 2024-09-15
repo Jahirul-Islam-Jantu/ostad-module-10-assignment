432131daed8cda0c4a8f7812a3fec975890b24a6
Jahirul Islam <jahirulislamjantu002@gmail.com>
2024-09-15 16:34:06 +0600

    2 problems

--- index.js
+++ index.js
@@ -1,4 +1,19 @@
-function calculateDifference(a, b){
-    return a - b
+//Write a function named calculateDifference that takes two arguments and returns the difference between the first and second argument.
+function calculateDifference(a, b) {
+  return a - b;
 }
-calculateDifference(2, 3)
+const result = calculateDifference(10, 6);
+console.log(result);
+
+// Write a function named isOdd that takes one argument and returns true if the number is odd, and false if it is not.
+function isOdd(number) {
+  return number % 2 !== 0;
+}
+console.log(isOdd(4));
+
+// Write a function named findMin that takes an array of numbers and returns the smallest number in the array.
+const numbers = [50, 2, 35, 44, 1, 234];
+function findMin() {
+  return numbers.sort;
+}
+findMin();
